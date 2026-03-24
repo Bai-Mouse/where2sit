68fb9f53464e58580fc44358e4ee4ecba783e119
Bai-Mouse <yixuanshi059@gmail.com>
2026-03-23 21:51:08 -0400

    Update reservation.spec.js

--- frontend/test/reservation.spec.js
+++ frontend/test/reservation.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('user can reserve a room', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:8000');
 
   await page.click('text=Reserve');
   
